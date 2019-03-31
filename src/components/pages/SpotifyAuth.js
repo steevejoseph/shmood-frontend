d8b9677bb05906688cf0a1f5912d5e6ad1d625c6
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-03-31 15:22:46 -0400

    Add moody video to home

--- src/components/pages/SpotifyAuth.js
+++ src/components/pages/SpotifyAuth.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import livemusic from '../../assets/vid/livemusic.mp4';
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 export default class SpotifyAuth extends Component {
   render() {
     return (
       <div>
+        <video width={'100%'} autoPlay loop>
+          <source src={livemusic} type="video/mp4"></source>
+        </video>
         <a href={`${API_URL}/spotify/auth/login`}>
           <button type="button">Log in with Spotify</button>
         </a>
