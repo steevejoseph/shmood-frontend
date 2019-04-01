c91ba4d20b939a31859614d2b2d075497ca1b5ac
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-04-01 00:21:04 -0400

    Make Landing not so ugly to look at

--- src/components/pages/SpotifyAuth.js
+++ src/components/pages/SpotifyAuth.js
@@ -5,16 +5,47 @@ import livemusic from '../../assets/vid/livemusic.mp4';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+
+const styles = {
+  
+  content: {
+    position: 'absolute',
+    bottom: '50%',
+    background: 'rgba(0, 0, 0, 0.0)',
+    color: '#f1f1f1',
+    width: '100%',
+    padding: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button:{
+    width: 200,
+    fontSize: 18,
+    padding: 10,
+    border: 'none',
+    background: '#000',
+    color: '#fff',
+    background: 'rgba(0, 0, 0, 0.1)',
+    cursor: 'pointer',
+    borerRadius: 10
+  }
+}
+
 export default class SpotifyAuth extends Component {
   render() {
     return (
       <div>
-        <video width={'100%'} autoPlay loop>
+        <video width={'100%'} height={'100%'}  minHeight="100%" minWidth="100%" autoPlay loop>
           <source src={livemusic} type="video/mp4"></source>
         </video>
-        <a href={`${API_URL}/spotify/auth/login`}>
-          <button type="button">Log in with Spotify</button>
-        </a>
+        <div style={styles.content}>
+          <a href={`#`}>
+        {/* <a href={`${API_URL}/spotify/auth/login`}> */}
+            <button style={styles.button}type="button">join the fun</button>
+          </a>
+        </div>
+
       </div>
     );
   }
