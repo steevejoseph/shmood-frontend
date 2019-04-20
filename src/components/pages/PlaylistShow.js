6dc8bb9ba61bc8a0d3647889dd1f2fe5a5f7013e
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-04-19 22:07:16 -0400

    Make PlaylistShow a little nicer

--- src/components/pages/PlaylistShow.js
+++ src/components/pages/PlaylistShow.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import Helmet from 'react-helmet';
 import Spotify from 'spotify-web-api-js';
+import Spinner from 'react-spinkit';
 import { getUserData, refreshTokensIfExpired } from '../../assets/scripts/spotify/auth';
-import { PlaylistCard } from '../common';
+import { PlaylistCard, SideNav, TopNav } from '../common';
 
 const spotify = new Spotify();
 
+const styles = {
+  screenDiv: {
+    marginTop: 50,
+  },
+};
 export default class PlaylistShow extends Component {
   state = {
     playlist: null,
@@ -42,12 +48,29 @@ export default class PlaylistShow extends Component {
 
   renderPlaylist() {
     if (!this.state.playlist || this.state.loading) {
-      return <div>loading...</div>;
+      return (
+        <div>
+          <Spinner name="line-scale" color="white" />
+        </div>
+      );
     }
+
     return <PlaylistCard playlist={this.state.playlist} />;
   }
 
   render() {
-    return <div>{this.renderPlaylist()}</div>;
+    return (
+      <div>
+        <Helmet>
+          <style>{'body { background-color: #141719; }'}</style>
+          {/* <style>{`body { background-color:${this.state.bgcol}; transition: 5000ms ease; }`}</style> */}
+        </Helmet>
+        <div className="container-fluid" style={{ display: 'flex', paddingTop: 50 }}>
+          <TopNav />
+          <SideNav />
+          <div style={styles.screenDiv}>{this.renderPlaylist()}</div>
+        </div>
+      </div>
+    );
   }
 }
