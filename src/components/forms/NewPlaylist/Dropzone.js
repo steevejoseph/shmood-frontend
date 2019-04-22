c43213c2685e8411d218f218546a26a7e68f1733
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-04-22 12:07:15 -0400

    Make new Dropzone component

---
+++ src/components/forms/NewPlaylist/Dropzone.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+
+const renderDropzoneInput = field => {
+  const files = field.input.value;
+  const [binString, setBinString] = useState('');
+
+  return (
+    <div>
+      <Dropzone
+        name={field.name}
+        onDrop={(filesToUpload, e) => {
+          field.input.onChange(filesToUpload);
+          console.log(filesToUpload);
+
+          const reader = new FileReader();
+          filesToUpload.forEach(file => reader.readAsDataURL(file));
+
+          reader.onabort = () => console.log('file reading was aborted');
+          reader.onerror = () => console.log('file reading has failed');
+          reader.onload = () => {
+            // Do whatever you want with the file contents
+            const binaryStr = reader.result;
+            setBinString(binaryStr);
+            // console.log(binString);
+          };
+        }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag your photo here (or click to select photo)</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+      {field.meta.touched && field.meta.error && <span className="error">{field.meta.error}</span>}
+      {files && Array.isArray(files) && (
+        // <ul>
+        //   {files.map((file, i) => (
+        //     <li key={i}>{file.name}</li>
+        //   ))}
+        // </ul>
+        <div>
+          <p>Photo preview</p>
+          <img src={binString} alt="img" width="75%" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default renderDropzoneInput;
